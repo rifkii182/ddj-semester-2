cad7c5a98d286ce4f067213a4c9b89dedd3ae6b7
rifkii182 <158979086+rifkii182@users.noreply.github.com>
2024-02-05 09:24:55 +0700

    Add files via upload

---
+++ pertemuan4/bilangan bulat.py
@@ -0,0 +1,10 @@
+x = 6
+
+if (x > 0):
+    print ("bilangan tersebut adalah bilangan positif")
+elif(x > 0):
+    print ("bilangan tersebut adalah bilangan negatif")
+elif(x == 0):
+    print ("bilangan tersebut adalah 0")
+else:
+    print("masukan angka yang benar") 
---
+++ pertemuan4/bilangan.py
@@ -0,0 +1,6 @@
+x = 9
+
+if (x % 2 == 0):
+    print("bilangan genap")
+else:
+    print("bilangan ganjil")
