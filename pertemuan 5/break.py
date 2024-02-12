72067321ebae95be45dcc46bbc5372c9d9be7558
rifkii182 <158979086+rifkii182@users.noreply.github.com>
2024-02-12 09:47:18 +0700

    Add files via upload

---
+++ pertemuan 5/break.py
@@ -0,0 +1,9 @@
+print("\nLOOPING IN PYTHON")
+print("--------------------\n")
+a = 0
+b = float(input("masukan angka anda"))
+while a < b: # a < b adalah syarat
+    a += 1 # incerement
+    if a == 5: # seleksi kondisi
+        continue # continue point
+    print (a)
